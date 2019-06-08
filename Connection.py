f28fe08ee2e5aa7ed7be56381643571bafaa1f23
EricLiuCY <ericliu7722@gmail.com>
2019-06-08 17:39:55 -0400

    please

--- Connection.py
+++ Connection.py
@@ -94,7 +94,7 @@ def connect():
     return s.makefile('rw', 1)
 
 def write_to_exchange(exchange, obj):
-    time.sleep(0.1)
+    time.sleep(0.3)
     json.dump(obj, exchange)
     exchange.write("\n")
 
