a38d3218c611c914651cc5dc1d05812e23b5f26f
Shivansh Vij <shivanshvij@outlook.com>
2019-06-08 13:19:58 -0400

    TEST

--- bond_trader.py
+++ bond_trader.py
@@ -13,7 +13,7 @@ from bond import bond_pricing
 team_name="loremipsum"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = True
+test_mode = False
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
