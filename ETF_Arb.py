a473acd402a7cadfda4953f4b57cdded8630fe4c
EricLiuCY <ericliu7722@gmail.com>
2019-06-08 18:22:27 -0400

    work

--- ETF_Arb.py
+++ ETF_Arb.py
@@ -3,10 +3,10 @@ import get_EMA as EMA
 
 def etf_decision(etf, bond, gs, ms, wfc):
 
-    bond_fair = EMA.get_EMA(bond[0], bond[1])
-    gs_fair = EMA.get_EMA(gs[0], gs[0])
-    ms_fair = EMA.get_EMA(ms[0], ms[0])
-    wfc_fair = EMA.get_EMA(wfc[0], wfc[0])
+    bond_fair = EMA.get_EMA(bond[0], bond[1], bond[2])
+    gs_fair = EMA.get_EMA(gs[0], gs[1], gs[2])
+    ms_fair = EMA.get_EMA(ms[0], ms[1], ms[2])
+    wfc_fair = EMA.get_EMA(wfc[0], wfc[1], wfc[2])
 
     etf_fair = 3*bond_fair + 2*gs_fair + 3*ms_fair + 2*wfc_fair + 100 + 10
     etf_to_buy = etf[0]
@@ -17,4 +17,6 @@ def etf_decision(etf, bond, gs, ms, wfc):
     elif etf_to_sell < etf_fair:
         return "SELL", EMA
     else:
-        return "NOTHING", EMA
+        return "NOTHING", etf_fair, bond_fair, gs_fair, ms_fair, wfc_fair
+
+
