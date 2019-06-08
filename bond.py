9e9fc10bc053d463bc21f7e03f162fcf0403d418
EricLiuCY <ericliu7722@gmail.com>
2019-06-08 11:57:48 -0400

    changed strategy

--- bond.py
+++ bond.py
@@ -1,28 +1,26 @@
 # Decision for the market
 
 
-def bond_pricing(buy_price, sell_price, min_profit):
-    print("Decision got buy at " + str(buy_price) + " and sell at " + str(sell_price))
+def bond_pricing(buy_prices, sell_prices, min_profit):
+    print("Decision got buy at " + str(buy_prices) + " and sell at " + str(sell_prices))
     fair_price = 1000
 
     buy_profit = 0
     short_profit = 0
 
-    if buy_price < fair_price - min_profit:
-        buy_profit = fair_price - buy_price
-        if buy_profit <= min_profit:
-            return "NOTHING", 0
-    elif sell_price > fair_price + min_profit:
-        short_profit = sell_price - fair_price
-        if short_profit <= min_profit:
-            return "NOTHING", 0
+    for buy_price in buy_prices:
+        if buy_price < fair_price - min_profit:
+            buy_profit = fair_price - buy_price
+
+    for sell_price in sell_prices:
+        if sell_price > fair_price + min_profit:
+            short_profit = sell_price - fair_price
+
+    if buy_profit > short_profit & buy_profit > 0:
+        return "BUY", buy_profit
+    elif short_profit > 0:
+        return "SELL", short_profit
     else:
         return "NOTHING", 0
-
-
-    if buy_profit > short_profit:
-        return "BUY", buy_profit
-    else:
-        return "SELL", short_profit
 
 
