f3e4b5f992657a5a5298f9c1783b0ab12fda2425
EricLiuCY <ericliu7722@gmail.com>
2019-06-08 10:58:57 -0400

    Update Bond Decision

--- bond.py
+++ bond.py
@@ -1,26 +1,21 @@
 # Decision for the market
 
 
-def bond_pricing(cur_price, min_profit):
+def bond_pricing(buy_price, sell_price, min_profit):
     fair_price = 1000
 
-    decision = None
-    est_profit = None
+    buy_profit = None
+    short_profit = None
 
-    if cur_price < fair_price - min_profit:
-        decision = "buy"
-        est_profit = fair_price - cur_price
+    if buy_price < fair_price - min_profit:
+        buy_profit = fair_price - buy_price
 
-    if cur_price > fair_price + min_profit:
-        decision = "sell"
-        est_profit = cur_price - fair_price
+    if sell_price > fair_price + min_profit:
+        short_profit = sell_price - fair_price
 
-    return decision, est_profit
-
-
-
-
-
-
+    if buy_profit > short_profit:
+        return "BUY", buy_profit
+    else:
+        return "SELL", short_profit
 
 
