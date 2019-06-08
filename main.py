74008ac8bb61b9fdc8b455beed2ef5b3ea9aeb54
EricLiuCY <ericliu7722@gmail.com>
2019-06-08 14:47:30 -0400

    update

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ ID_array = []
 bond_buy_count = 50
 bond_sell_count = 50
 
+xlf_buy, xlf_sell, xlf_ema = None
+bond_buy, bond_sell = None
+gs_buy, gs_sell = None
+ms_buy, ms_sell = None
+wfc_buy, wfc_sell = None
+
 def generate_ID():
 
     ID = random.randint(1, 1000)
@@ -56,7 +62,7 @@ def read_from_exchange(exchange):
     return json.loads(exchange.readline())
 
 
-# ~~~~============== My Code ================ ~~~~~~~
+# ~~~~============== BONDS ================ ~~~~~~~
 
 
 def bond_market_maker(exchange):
@@ -66,6 +72,16 @@ def bond_market_maker(exchange):
 
     write_to_exchange(exchange,
                       {"type": "add", "order_id": generate_ID(), "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 50})
+
+    print("BOND: INITIAL MARKET MADE")
+
+def bond_buy_25(exchange):
+    write_to_exchange(exchange,
+                      {"type": "add", "order_id": generate_ID(), "symbol": "BOND", "dir": "BUY", "price": 999, "size": 25})
+
+def bond_sell_25(exchange):
+    write_to_exchange(exchange,
+                      {"type": "add", "order_id": generate_ID(), "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 25})
 
 # ~~~~============== Trash =============== ~~~~~~~
 
@@ -84,127 +100,45 @@ def buy_bond(exchange, start_position, ORDERS, prices):
                       {"type": "add", "order_id": SORDERID, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": SELLNUM})
     sreturn_exchange = read_from_exchange(exchange)
 
-    def buyer(return_exchange, BUYNUM, ORDERID, SORDERID):
-
-        if BUYNUM < 50:
-            write_to_exchange(exchange,
-                              {"type": "add", "order_id": ORDERID, "symbol": "BOND", "dir": "BUY", "price": 999,
-                               "size": (50 - BUYNUM)})
-            ORDERID += 1
-            print("BUY ORDER PLACED OF SIZE: " + str(50 - BUYNUM))
-            BUYNUM = 50
-            return read_from_exchange(exchange), BUYNUM, ORDERID
-        if return_exchange['type'] == 'ack':
-            if return_exchange['order_id'] == ORDERID:
-                print("BUY ORDER ACKNOWLEDGED")
-                return read_from_exchange(exchange), BUYNUM, ORDERID
-        if return_exchange['type'] == 'book':
-            buys = return_exchange['buy']
-            sells = return_exchange['sell']
-
-            avg_buy = -1
-            avg_sell = -1
-
-            max_buy = 0
-            max_sell = 0
-            for i in buys:
-                max_buy = max(max_buy, i[1])
-                if max_buy == i[1]:
-                    avg_buy = i[0]
-            for i in sells:
-                max_sell = max(max_sell, i[1])
-                if max_sell == i[1]:
-                    avg_sell = i[0]
-            prices[return_exchange['symbol']] = (avg_buy, avg_sell, None)
-        elif return_exchange['type'] == 'fill':
-            if return_exchange['price'] == 999:
-                BUYNUM -= return_exchange['size']
-                print("BUY ORDER FULFILLED AT PRICE: " + str(return_exchange['price']) + " OF SIZE: " + str(
-                    return_exchange['size']))
-                ORDERID+=1
-            elif return_exchange['order_id'] == SORDERID:
-                return return_exchange, SELLNUM, ORDERID
-            return read_from_exchange(exchange), BUYNUM, ORDERID
-        return read_from_exchange(exchange), BUYNUM, ORDERID
-
-    def seller(return_exchange, SELLNUM, ORDERID, SORDERID):
-        if SELLNUM < 50:
-            write_to_exchange(exchange,
-                              {"type": "add", "order_id": ORDERID, "symbol": "BOND", "dir": "SELL", "price": 1001,
-                               "size": (50 - SELLNUM)})
-            ORDERID += 1
-            print("SELL ORDER PLACED OF SIZE: " + str(50-SELLNUM))
-            SELLNUM = 50
-
-            return read_from_exchange(exchange), SELLNUM, ORDERID
-        if return_exchange['type'] == 'book':
-            buys = return_exchange['buy']
-            sells = return_exchange['sell']
-
-            avg_buy = -1
-            avg_sell = -1
-
-            max_buy = 0
-            max_sell = 0
-            for i in buys:
-                max_buy = max(max_buy, i[1])
-                if max_buy == i[1]:
-                    avg_buy = i[0]
-            for i in sells:
-                max_sell = max(max_sell, i[1])
-                if max_sell == i[1]:
-                    avg_sell = i[0]
-            prices[return_exchange['symbol']] = (avg_buy, avg_sell, None)
-        if return_exchange['type'] == 'ack':
-            if return_exchange['order_id'] == ORDERID:
-                print("SELL ORDER ACKNOWLEDGED")
-                return read_from_exchange(exchange), SELLNUM, ORDERID
-        elif return_exchange['type'] == 'fill':
-            if return_exchange['price'] == 1001:
-                SELLNUM -= return_exchange['size']
-                print("SELL ORDER FULFILLED AT PRICE: " + str(return_exchange['price']) + " OF SIZE: " + str(
-                    return_exchange['size']))
-                ORDERID+=1
-            elif return_exchange['order_id'] == SORDERID:
-                return return_exchange, SELLNUM, ORDERID
-            return read_from_exchange(exchange), SELLNUM, ORDERID
-        return read_from_exchange(exchange), SELLNUM, ORDERID
-
-    while True:
-        return_exchange, BUYNUM, ORDERID = buyer(return_exchange, BUYNUM, ORDERID, SORDERID)
-        sreturn_exchange, SELLNUM, SORDERID = seller(sreturn_exchange, SELLNUM, SORDERID, ORDERID)
-
 
 def dispatcher(exchange):
     message = read_from_exchange(exchange)
     type = message['type']
+    global bond_buy_count, bond_sell_count
 
     if type == "book":
         print("it works hahahah")
+
+    # BOND
+    elif type == "fill":
+        symbol = message['symbol']
+        if symbol == "BOND":
+            if message['price'] == 999:
+                bond_buy_count -= 1
+                if bond_buy_count <= 25:
+                    bond_buy_25(exchange)
+                    print("BOND: BUY order placed")
+            if message['price'] == 1001:
+                bond_sell_count -= 1
+                if bond_sell_count <= 25:
+                    bond_sell_25(exchange)
+                    print("BOND: SELL order placed")
+
+    elif type == "ack":
+        print("ack")
+    elif type == "reject":
+        print("rejected")
 
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
 def main():
-    prices = OrderedDict()
-    ORDERS = 0
     exchange = connect()
     write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
-    start_position = read_from_exchange(exchange)
-
     bond_market_maker(exchange)
 
     while True:
         dispatcher(exchange)
-
-    # try:
-    #     while True:
-    #         buy_bond(exchange, start_position, ORDERS, prices)
-    # except KeyboardInterrupt:
-    #     # f = open('BOND_HISTORY', 'w')
-    #     # pickle.dump(repr(ORDERS), f)
-    #     # f.close()
-    #     pass
 
 if __name__ == "__main__":
     while True:
