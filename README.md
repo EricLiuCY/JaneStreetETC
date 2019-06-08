ca688d67a305e7b53f479617fa496e4cd8c081e3
EricLiuCY <ericliu7722@gmail.com>
2019-06-08 11:33:35 -0400

    Temp

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Jane Street Eletronic Trading Competition
 
 
 Members: Eric Liu, Felix He, Shivansh Vij:
+
+
+Code Flow:
+
+1. Connection, receive data from server
+2. Parse from universal parser
+3. Pass parsed info into a decision dispatcher
+4. Take the returned the decision and pass to JSON generator
+5. JSON generator sends JSON over to be sent to server. 
+- C
--- bond.py
+++ bond.py
@@ -12,11 +12,13 @@ def bond_pricing(buy_price, sell_price, min_profit):
         buy_profit = fair_price - buy_price
         if buy_profit <= min_profit:
             return "NOTHING", 0
-
-    if sell_price > fair_price + min_profit:
+    elif sell_price > fair_price + min_profit:
         short_profit = sell_price - fair_price
         if short_profit <= min_profit:
             return "NOTHING", 0
+    else:
+        return "NOTHING", 0
+
 
     if buy_profit > short_profit:
         return "BUY", buy_profit
